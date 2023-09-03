afc2ef00dc06c0879d88bf66edb9955dbd88b6a7
Meta Lekan <metadelekan@gmail.com>
2023-09-03 08:03:09 +0100

    new update

--- db/connect.js
+++ db/connect.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://meta:john5646@nodetutorial.n6pauji.mongodb.net/03-Task-Manager?retryWrites=true&w=majority";
 
-const connectDB = (url) => {
-  return mongoose
+mongoose
   .connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -13,8 +12,3 @@ const connectDB = (url) => {
   })
   .then(() => console.log("Connect to MongoDB"))
   .catch((error) => console.log(error));
-
-}
-
-module.exports = connectDB;
-
