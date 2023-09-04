7148a3525b172c6b969f05da9c262a356b04bb4f
Availeth <132179400+Availeth@users.noreply.github.com>
2023-09-04 10:35:37 -0400

    fix

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://meta:john5646@nodetutorial.n6pauji.mongodb.net/03-Task-Manager?retryWrites=true&w=majority";
+// const connectionString =
+//   "mongodb+srv://meta:john5646@nodetutorial.n6pauji.mongodb.net/03-Task-Manager?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Connect to MongoDB"))
-  .catch((error) => console.log(error));
+// mongoose
+//   .connect(connectionString, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("Connect to MongoDB"))
+//   .catch((error) => console.log(error));
